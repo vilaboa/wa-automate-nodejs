cf6bfb8151147746bfce616926693ebe6b6d0e97
Gabriel <gabriel@vilaboa.cl>
2025-03-23 02:26:07 -0300

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { create, start } from './controllers/initializer';
+
+create({
+  apiHost: '0.0.0.0',
+  apiPort: parseInt(process.env.PORT || '3000'),
+  apiKey: process.env.API_KEY || '1234',
+  headless: true
+}).then(client => start(client));
